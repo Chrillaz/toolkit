1f3b8bee4dbbf4221d937e5e854a34e70a9d6677
Christoffer Öhman <totalchriz@gmail.com>
2022-09-17 01:00:03 +0200

    lint script

---
+++ packages/scripts/scripts/lint.js
@@ -0,0 +1,51 @@
+const { sync: spawn } = require('cross-spawn');
+const { sync: resolveBin } = require('resolve-bin');
+const { hasArg, getArgs, getFileArg } = require('../utilities/cli');
+const { hasConsumerConfiguration, getPackageConfiguration } = require('../utilities');
+
+const { scriptArgs } = getArgs();
+
+const hasEslintConfig = () =>
+	hasArg('config') || hasArg('c') || hasConsumerConfiguration('.eslint');
+
+const configArgs = hasEslintConfig()
+	? []
+	: [
+			'--config',
+			getPackageConfiguration('.eslint'),
+			'--no-eslintrc',
+	  ];
+
+const hasEslintIgnore = () => hasArg('--ignore-path') || hasConsumerConfiguration('.eslintignore');
+
+const ignoreArgs = hasEslintIgnore()
+	? []
+	: [
+			'--ignore-path',
+			getPackageConfiguration('.eslintignore'),
+	  ];
+
+const extArgs = hasArg('ext')
+	? []
+	: [
+			'--ext',
+			'js,jsx,ts,tsx',
+	  ];
+
+const fileArgs = getFileArg().length > 0 ? [] : ['.'];
+
+const { status } = spawn(
+	resolveBin('eslint'),
+	[
+		...configArgs,
+		...ignoreArgs,
+		...extArgs,
+		...scriptArgs,
+		...fileArgs,
+	],
+	{
+		stdio: 'inherit',
+	}
+);
+
+process.exit(status || 1);
